12b98b0682ed87a776cc3f6a9b44f752f71c7a2f
Greg Witt <36649901+GoodGuyGregory@users.noreply.github.com>
2021-04-01 18:47:43 -0400

    Adds Data Binding

---
+++ Wpf_DataBinding/Wpf_DataBinding/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_DataBinding
+{
+     /// <summary>
+     /// Interaction logic for MainWindow.xaml
+     /// </summary>
+     public partial class MainWindow : Window
+     {
+          public MainWindow()
+          {
+               InitializeComponent();
+               // One Time to Source is used for Onetime Binding
+               Slider4.Value = 30;
+               // Establish the Text value upon initalization 
+               MyTextBox.Text = Slider4.Value.ToString();
+          }
+     }
+}
