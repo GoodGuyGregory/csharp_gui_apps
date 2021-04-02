0b8f8142f842307181dca14ecdc9ca34973453ee
Greg Witt <36649901+GoodGuyGregory@users.noreply.github.com>
2021-04-02 13:18:14 -0400

    Add files via upload

---
+++ Wpf_INotifyProperty/Wpf_INotifyProperty/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_INotifyProperty
+{
+     /// <summary>
+     /// Interaction logic for MainWindow.xaml
+     /// </summary>
+     public partial class MainWindow : Window
+     {
+          // creates an instance of the Sum class in the window for the Code to reference the sum behavior
+          public Sum SumObj { get; set; }
+
+          public MainWindow()
+          {
+               InitializeComponent();
+
+               // initialize the sum object with a starting value
+               SumObj = new Sum { Num1 = "1", Num2 = "3" };
+               this.DataContext = SumObj;
+
+
+          }
+     }
+}
---
+++ Wpf_INotifyProperty/Wpf_INotifyProperty/Sum.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Wpf_INotifyProperty
+{
+     public class Sum : INotifyPropertyChanged
+
+     {
+          private string num1;
+          private string num2;
+          private string result;
+
+          public string Num1 {
+               get { return num1; }
+               set { int number;
+                    // try parse will attempt to convert the value to an integer and assign it to value
+                    bool res = int.TryParse(value, out number);
+                    if (res) {
+                         num1 = value;
+                    }
+                    OnPropertyChanged("Num1");
+                    OnPropertyChanged("Result");
+               }
+          }
+
+          public string Num2
+          {
+               get { return num2; }
+               set
+               {
+                    int number;
+                    // try parse will attempt to convert the value to an integer and assign it to value
+                    bool res = int.TryParse(value, out number);
+                    if (res)
+                    {
+                         num2 = value;
+                    }
+                    OnPropertyChanged("Num2");
+                    OnPropertyChanged("Result");
+               }
+          }
+
+          public string Result
+          {
+               get {
+                    int res = int.Parse(Num1) + int.Parse(Num2);
+                    return res.ToString();
+               }
+
+               set {
+                    int res = int.Parse(Num1) + int.Parse(Num2);
+                    result = res.ToString();
+                    OnPropertyChanged("Result");
+               }
+
+          }
+          // this allows C# to change the attributes of the property inside of our onCh anged Method
+          public event PropertyChangedEventHandler PropertyChanged;
+
+          private void OnPropertyChanged(string property) {
+               if (PropertyChanged != null) {
+                    PropertyChanged(this, new PropertyChangedEventArgs(property));
+                }
+          }
+     }
+}
