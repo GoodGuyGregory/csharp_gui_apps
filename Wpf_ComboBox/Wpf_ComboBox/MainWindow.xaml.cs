e857dc915085df8bcc9efc64c338fd27a50a37ce
Greg Witt <36649901+GoodGuyGregory@users.noreply.github.com>
2021-04-02 14:53:04 -0400

    Add files via upload

---
+++ Wpf_ComboBox/Wpf_ComboBox/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_ComboBox
+{
+     /// <summary>
+     /// Interaction logic for MainWindow.xaml
+     /// </summary>
+     public partial class MainWindow : Window
+     {
+          public MainWindow()
+          {
+               InitializeComponent();
+
+               // Access the Combobox Colors
+               // Setting the items in the combobox can be done by assigning the values you want with ItemSource
+               comboBoxColors.ItemsSource = typeof(Colors).GetProperties();
+          }
+     }
+}
